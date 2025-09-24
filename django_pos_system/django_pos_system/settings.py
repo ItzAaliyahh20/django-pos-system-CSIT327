72e186a70ab6df5da9d26951991cad5fa9fca898
LAB13_USER ON WS25 <L13X16W25@LabHCI.net>
2025-09-24 16:58:34 +0800

    feature(app_name): setup templates

--- django_pos_system/django_pos_system/settings.py
+++ django_pos_system/django_pos_system/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pos_app',
+    'pos_app'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django_pos_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates/pos_app'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
